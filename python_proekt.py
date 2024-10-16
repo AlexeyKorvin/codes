e31b0f95848f41b31be54ec967a41d5951eac7d0
AlexeyKorvin <korvinalex2020@gmail.com>
2024-10-16 18:24:18 +0300

    Add files via upload

---
+++ python_proekt.py
@@ -0,0 +1,63 @@
+"""age = int(input())
+if age > 14:
+    print("Добро пожаловать!")
+elif age < 14:
+    print("Вам ещё рано!")
+elif age == 14:
+    print("Вам 14 лет! Поздравляю!")"""
+
+"""password = input()
+if password == "secret":
+    print("Доступ разрешен!")
+else:
+    print("Неверный пароль!")"""
+
+"""a = input()
+if a.isalpha():
+    print("Это буква!")
+elif a.isdigit():
+    print( "Это цифра!")
+else:
+    print( "Это не цифра и не буква!")"""
+
+
+
+"""a = int(input())
+if a > 1000:
+    print(a*0.9)
+elif a > 500:
+    print(a*0.95)
+else:
+    print(a)"""
+
+"""Sum = 0
+for i in range(5):
+    a = int(input())
+    Sum += a
+print(Sum)"""
+
+"""n = int(input())
+f = 1
+while n>1:
+    f *= n
+    n -= 1
+print(f)"""
+
+"""a = int(input())
+k = 0
+for i in range(2, a//2 + 1):
+    if a%i==0:
+        k += 1
+if k<=0:
+    print("Число простое")
+else:
+    print("Число не простое")"""
+    
+
+
+
+
+
+
+    
+    
